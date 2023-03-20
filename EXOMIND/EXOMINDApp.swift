aca33da7cf1981c74f0b742c0b1ad7ce6812230a
Aissatou <aissatouabdoul.diallo@gmail.com>
2023-03-20 21:01:37 +0100

    Initial Commit

---
+++ EXOMIND/EXOMINDApp.swift
@@ -0,0 +1,17 @@
+//
+//  EXOMINDApp.swift
+//  EXOMIND
+//
+//  Created by aistou 💕 on 20/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct EXOMINDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
